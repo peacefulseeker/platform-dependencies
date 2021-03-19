725ee8bdac1008b2e8894cba766e1089ea706711
Alexey Vorobyov <alexey.vorobyov@prezi.com>
2021-03-19 06:39:46 +0200

    INT Bite 146. Rhombus

---
+++ 146/rhombus.py
@@ -0,0 +1,37 @@
+STAR = '*'
+
+def gen_rhombus(width):
+    """Create a generator that yields the rows of a rhombus row
+       by row. So if width = 5 it should generate the following
+       rows one by one:
+
+       gen = gen_rhombus(5)
+       for row in gen:
+           print(row)
+
+        output:
+          * width - 4, row 1
+         ***, with - 2, row 2
+        *****, width
+         ***, width - 2, row 4
+          *, width - 4, row 5
+    """
+
+    def _get_stars(stars_count):
+        return f"{STAR * stars_count : ^{width}}"
+
+    stars_count = 1
+
+    for row in range(1, width + 1):
+        # the most middle:
+        if (width // 2 + 1) == row:
+            yield(_get_stars(stars_count))
+            stars_count -= 2
+        elif row > width / 2:
+            yield(_get_stars(stars_count))
+            stars_count -= 2
+        else:
+            yield(_get_stars(stars_count))
+            stars_count += 2
+
+gen_rhombus(11)
